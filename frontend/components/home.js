b6d189143eaf6110803505b05cd9bdbaf413fecb
saadathussain586 <saadathussainofficial.com>
2022-07-19 17:25:01 +0500

    push

---
+++ frontend/components/home.js
@@ -0,0 +1,13 @@
+function Home() {
+    return < >
+        <
+        div className = "container" >
+        <
+        div >
+        <
+        h1 > Basic Crud App with MERN + jwt auth < /h1> <
+        /div>  <
+        /div>  <
+        />
+}
+export default Home
